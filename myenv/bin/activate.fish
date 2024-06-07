bb236ab92724c19c978abe07aa64d11fa2a3412e
pharsha9 <116417608+pharsha9@users.noreply.github.com>
2024-06-07 14:12:56 +0530

    tr2

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/harshavardhanreddy/Downloads/major/myenv"
+set -gx VIRTUAL_ENV "/Users/harshavardhanreddy/mini/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
